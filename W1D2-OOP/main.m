38c1cdaffad37b88bac3055baa9f744bd8b852f8
Daniel Mathews <dannyl.mathews@gmail.com>
2017-05-30 07:36:09 -0700

    Initial Commit

---
+++ W1D2-OOP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W1D2-OOP
+//
+//  Created by Daniel Mathews on 2017-05-30.
+//  Copyright © 2017 Daniel Mathews. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
